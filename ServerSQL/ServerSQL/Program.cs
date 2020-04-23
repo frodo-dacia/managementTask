415986d44a79c336d513630f122228f0c37ade96
Diana <sandudianaelena2@gmail.com>
2020-04-23 13:26:05 +0300

    Server

---
+++ ServerSQL/ServerSQL/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread t = new Thread(delegate ()
+        {
+
+            Server myserver = new Server("192.168.56.1", 13000);
+        });
+        t.Start();
+
+        Console.WriteLine("Server Started...!");
+
+    }
+}
